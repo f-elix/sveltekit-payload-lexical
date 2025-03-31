f6c4fb6145bea86585a78922eb85ae12c60de144
f-elix <guerinfelix08@gmail.com>
2025-03-31 10:25:36 -0400

    Actually use package

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
+<script lang="ts">
+	import type { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical';
+	import {
+		convertLexicalToHTML,
+		type HTMLConverters,
+		type HTMLConvertersFunction
+	} from '@payloadcms/richtext-lexical/html';
+
+	const html = convertLexicalToHTML({
+		data: {},
+		className: `_rich-text ${typeClasses[type]}`,
+		converters: ({ defaultConverters }) => {
+			return {
+				...defaultConverters,
+				...(converters ?? {})
+			};
+		},
+		disableTextAlign: true
+	});
+</script>
+
+{@html html}
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
